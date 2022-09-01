1f657f2e2b8bfc5d7eacc200e612afe15328b1ae
Porras-Junco Oscar Eduardo <oscar-eduardo.porras-junco@insa-rennes.fr>
2022-09-01 17:12:09 +0200

    First version of Dataset class, the images imported have a BLACK BACKGROUND.

---
+++ dataset.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+from digit import Digit
+from sklearn.preprocessing import StandardScaler
+
+
+class Dataset:
+    def __init__(self, data, size=0):
+
+        self.length = int((len(data['data'])))
+        if 0 < size < self.length:
+            self.length = size
+        else:
+            size = self.length
+        self.scaler = StandardScaler()
+        self.targets = data['target'][0:size]
+        self.data = data['data'].to_numpy()[0:size]
+        self.digits = []
+        self.create_digits()
+        self.X_train = []
+        self.y_train = []
+        self.X_test = []
+        self.y_test = []
+
+    def create_digits(self):
+        for i in range(self.length):
+            self.digits.append(Digit(self.data[i], self.targets[i]))
+
+    def print_info(self):
+        from collections import Counter
+
+        c = Counter(self.targets)
+        info = "Dataset size " + str(self.length)
+        key_value = {}
+        for i in sorted(c.keys()):
+            key_value[i] = c[i]
+        plt.interactive(False)
+        plt.bar(key_value.keys(), key_value.values())
+        plt.xlabel('Labels')
+        plt.ylabel('Occurrence')
+        plt.title('Occurrence of MNIST dataset labels')
+        ax = plt.axes()
+        ax.grid(which='major', axis='y')
+        plt.show()
+        return info
+
+    def separate_train_test(self, test_size_ratio):
+        from sklearn.model_selection import train_test_split
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.data, self.targets,
+                                                                                test_size=test_size_ratio)
+        self.X_train = self.X_train / 255
+        self.X_test = self.X_test / 255
+        print('Size of training set: ' + str(len(self.y_train)) + '/' + str(len(self.data)))
+        print('Size of testing set: ' + str(len(self.y_test)) + '/' + str(len(self.data)))
+
+    def display_train_test(self):
+        from collections import Counter
+
+        test = Counter(self.y_test)
+        train = Counter(self.y_train)
+        info = "Dataset size: " + str(self.length)
+        print(info)
+
+        key_value_train = {}
+        key_value_test = {}
+
+        for i in sorted(test.keys()):
+            key_value_test[i] = test[i]
+        for i in sorted(train.keys()):
+            key_value_train[i] = train[i]
+
+        p1 = plt.bar(key_value_train.keys(), key_value_train.values(), width=0.5)
+        p2 = plt.bar(key_value_test.keys(), key_value_test.values(), width=0.5, bottom=list(key_value_train.values()))
+
+        plt.legend((p1[0], p2[0]), ('Training set', 'Test set'), loc='lower left')
+        plt.xlabel('Labels')
+        plt.ylabel('Occurrence')
+        plt.title('Occurrence of training and testing sets')
+        ax = plt.axes()
+        ax.grid(which='major', axis='y')
+        plt.show()
